376be723bcf167c3a75aa68700fe3860ef287a0e
dineshkush <kushwahd18@gmail.com>
2023-11-26 19:18:54 +0530

    create more pages

--- src/shared/request-call-back/RequestCallBack.js
+++ src/shared/request-call-back/RequestCallBack.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
 
 function RequestCallBack() {
   return (
-    <div>
-      <h1>Request Call Back</h1>
-    </div>
-  )
+    <>
+      <div className="popup_form">
+        <form>
+          <div className="row">
+            <div className="col-md-6">
+              <input
+                name="name"
+                type="text"
+                placeholder="Your Name"
+                required=""
+              />
+            </div>
+            <div className="col-md-6">
+              <input
+                name="email"
+                type="email"
+                placeholder="Email Address"
+                required=""
+              />
+            </div>
+            <div className="col-md-12">
+              <input
+                name="phone"
+                type="tel"
+                placeholder="Phone Number"
+                required=""
+              />
+            </div>
+            <div className="col-md-12">
+              <textarea
+                name="comments"
+                cols="40"
+                rows="3"
+                placeholder="Message..."
+                spellcheck="true"
+                required=""
+              ></textarea>
+            </div>
+            <div className="col-md-12">
+              <button type="submit" class="btn site_btn">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default RequestCallBack
+export default RequestCallBack;
